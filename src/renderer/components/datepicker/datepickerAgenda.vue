10a0e9d9583aa9418f0226d047cdd588ee198421
blaadje <acharlot91@gmail.com>
2017-08-09 15:27:41 +0200

    Fixing days

--- src/renderer/components/datepicker/datepickerAgenda.vue
+++ src/renderer/components/datepicker/datepickerAgenda.vue
@@ -6,9 +6,10 @@
   .days
     .weekdays(v-for="(day, index) in days", key="index")
       | {{day}}
-    .day(:style="{width: (month.getWeekStart() * 41) + 'px'}")
+    .spacer(:style="{width: (month.getWeekStart() * 41) + 'px'}")
     .day(v-for="day in month.getDays()")
-      | {{ day.format('DD') }}
+      span.overlay
+      span.text {{ day.format('D') }}
 </template>
 
 <script>
@@ -50,15 +51,35 @@ export default {
     .date
       font-size: 1.5em
   .days
+    width: 294px
     .weekdays
       text-align: center
       padding: 14px
       width: 41px
       display: inline-block
-    .day
-      width: 41px
+    .spacer
       height: 41px
       vertical-align: top
       text-align: center
       display: inline-block
+    .day
+      position: relative
+      cursor: pointer
+      height: 41px
+      width: 41px
+      vertical-align: top
+      text-align: center
+      display: inline-block
+      .overlay
+        position: absolute
+        top: -7px
+        left: 3px
+        height: 36px
+        width: 36px
+        border-radius: 50%
+        background: red
+      .text
+        position: relative
+        color: white
+
 </style>
