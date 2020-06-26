290b62f0dc4b1455d70b8e5288a7aafcc40b03cd
Alexandre <acharlot91@gmail.com>
2020-06-26 17:48:00 +0200

    refactor: Adding sentry (#22)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import * as Sentry from '@sentry/browser'
+import { Vue as VueIntegration } from '@sentry/integrations'
 import App from './components/App'
 import dotenv from 'dotenv'
 import vClickOutside from 'v-click-outside'
@@ -6,6 +8,13 @@ import vClickOutside from 'v-click-outside'
 Vue.use(vClickOutside)
 
 dotenv.config()
+
+Sentry.init({
+  dsn:
+    'https://5a61b511b9df49f28eacbd267bbe3b28@o412957.ingest.sentry.io/5294690',
+  integrations: [new VueIntegration({ Vue, attachProps: true })],
+  release: `todolist@${process.env.npm_package_version}`,
+})
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
